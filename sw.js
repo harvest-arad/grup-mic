5aa44a566357beb23f09bf562a223fb2c18b4554
valentinsfechis-dev <valentin.sfechis@gmail.com>
2026-09-21 21:29:09 +0300

    Grup mic: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Păstrează fișierele aplicației pe telefon; datele vin mereu de la server.
-const CACHE = 'grup-mic-v3';
+const CACHE = 'grup-mic-v4';
 const FILES = ['./', 'index.html', 'logo-mark.svg', 'manifest.webmanifest',
                'icon-192.png', 'icon-512.png', 'icon-maskable-512.png', 'apple-touch-icon.png'];
 const FONTURI = ['fonts.googleapis.com', 'fonts.gstatic.com'];
